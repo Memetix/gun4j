4f0b91480ba3a609fe53e7da73b570cd44888f0c
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-08-19 01:14:46 -0600

    Initial implementation of UrlExpandService

---
+++ src/main/java/com/memetix/gun4j/expand/UrlExpandService.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2011 Memetix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.memetix.gun4j.expand;
+
+import com.memetix.gun4j.GunshortenAPI;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ * MetricService
+ * Concrete implementation of the Chits Metric REST Service. Use this for CRUD ops on the Metric resource
+ * 
+ * @author Jonathan Griggs <jonathan.griggs @ gmail.com>
+ * @date Aug 18, 2011
+ */
+public class UrlExpandService extends GunshortenAPI {
+    private static final String SERVICE_URL = BASE_SERVICE_URL + "/expand?";
+    private static final String PARAM_NAME = "shortUrl";
+    
+    @Override
+    public String getServiceUrl() {
+        return SERVICE_URL;
+    }
+    
+    public String expand(final String shortUrl) throws Exception {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(PARAM_NAME);
+        sb.append(EQUALS);
+        sb.append(shortUrl);
+        
+        final Map<String,String> results = parseResponse(post(sb.toString()));
+        
+        if(results.containsKey(shortUrl))
+            return results.get(shortUrl);
+        else
+            return shortUrl;
+    }
+    
+    public Map<String,String> expand(final Set<String> shortUrls) throws Exception {
+        final StringBuilder sb = new StringBuilder();
+        
+        int i = 0;
+        for(String shortUrl : shortUrls) {
+            if(i>0) {
+                sb.append(AMPERSAND);
+            }
+            sb.append(PARAM_NAME);
+            sb.append(EQUALS);
+            sb.append(shortUrl);
+            i++;
+        }
+        
+        final Map<String,String> results = parseResponse(post(sb.toString()));
+        return results;
+    }
+    
+    public Map<String,String> expand(final List<String> shortUrls) throws Exception {
+        final Set<String> urlSet = new HashSet<String>();
+        urlSet.addAll(shortUrls);
+        return expand(urlSet);
+    }
+    
+    private Map<String,String> parseResponse(final JSONObject json) {
+        final Map<String,String> expandedResults = new HashMap<String,String>();
+        final JSONObject data = (JSONObject)json.get("data");
+        final JSONArray expand = (JSONArray)data.get("expand");
+        for(Object result : expand) {
+            final JSONObject expandedResult = (JSONObject)result;
+            expandedResults.put((String)expandedResult.get("shortUrl"), (String)expandedResult.get("fullUrl"));
+        }
+        return expandedResults;
+    }
+}
---
+++ src/test/java/com/memetix/gun4j/expand/UrlExpandServiceTest.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2011 Memetix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.memetix.gun4j.expand;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * Unit tests for the Gunshorten Expand service
+ * 
+ * @author Jonathan Griggs <jonathan.griggs @ gmail.com>
+ * @date Aug 18, 2011
+ */
+public class UrlExpandServiceTest {
+    
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    
+    public UrlExpandServiceTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getServiceUrl method, of class MetricService.
+     */
+    @Test
+    public void testGetServiceUrl() {
+        UrlExpandService instance = new UrlExpandService();
+        String expResult = System.getProperty("gunshorten.api.url")!=null ? System.getProperty("gunshorten.api.url") + "/expand?" : "http://gunshorten.cloudfoundry.com/api/expand?";
+        String result = instance.getServiceUrl();
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testExpandUrl() throws Exception {
+        UrlExpandService instance = new UrlExpandService();
+        String shortUrl = "http://t.co/UdEWcNm";
+        String result = instance.expand(shortUrl);
+        String expResult = "http://twitcaps.com/search?q=dust+storm";
+        assertNotNull(result);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testExpandUnshortUrl() throws Exception {
+        UrlExpandService instance = new UrlExpandService();
+        String shortUrl = "http://twitcaps.com/search?q=dust+storm";
+        String result = instance.expand(shortUrl);
+        String expResult = "http://twitcaps.com/search?q=dust+storm";
+        assertNotNull(result);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testExpandMultipleUrlsSet() throws Exception {
+        UrlExpandService instance = new UrlExpandService();
+        Set<String> shortUrls = new HashSet<String>();
+        
+        String shortUrl1 = "http://t.co/UdEWcNm";
+        String shortUrl2 = "http://t.co/g5GuXeQ";
+        
+        shortUrls.add(shortUrl1);
+        shortUrls.add(shortUrl2);
+        
+        Map<String,String> results = instance.expand(shortUrls);
+        String expResult1 = "http://twitcaps.com/search?q=dust+storm";
+        String expResult2 = "http://twitcaps.com/search?q=waterpop";
+        
+        assertNotNull(results);
+        assertEquals(2,results.size());
+        assertEquals(expResult1, results.get(shortUrl1));
+        assertEquals(expResult2, results.get(shortUrl2));
+    }
+    
+    @Test
+    public void testExpandMultipleUrlsList() throws Exception {
+        UrlExpandService instance = new UrlExpandService();
+        List<String> shortUrls = new ArrayList<String>();
+        
+        String shortUrl1 = "http://t.co/UdEWcNm";
+        String shortUrl2 = "http://t.co/g5GuXeQ";
+        String shortUrl3 = "http://t.co/g5GuXeQ";
+        
+        shortUrls.add(shortUrl1);
+        shortUrls.add(shortUrl2);
+        shortUrls.add(shortUrl3);
+        
+        Map<String,String> results = instance.expand(shortUrls);
+        String expResult1 = "http://twitcaps.com/search?q=dust+storm";
+        String expResult2 = "http://twitcaps.com/search?q=waterpop";
+        
+        assertNotNull(results);
+        assertEquals(2,results.size());
+        assertEquals(expResult1, results.get(shortUrl1));
+        assertEquals(expResult2, results.get(shortUrl2));
+        assertEquals(expResult2, results.get(shortUrl3));
+    }
+}
